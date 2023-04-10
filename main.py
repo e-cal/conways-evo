d9521137b3d8d982f436026b226f49e9bf5e9730
ecal <ethan@ecal.dev>
2023-04-10 11:02:56 -0400

    Add generational logging

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ EVAL_WINDOW = (40, 54)
 MAX_GENS = 1  # 400
 N_STEPS = EVAL_WINDOW[1]  # end when done evaluating
 
+DEFUALT_PATH = "history"
+
 
 def init():
     cells = np.random.randint(low=0, high=2, size=(POP_SIZE, NROWS, NCOLS))
@@ -53,7 +55,7 @@ def run(cells):
     return fitness
 
 
-def main(auto_save=False):
+def main(fp):
     population = init()
     fitnesses = [0] * POP_SIZE
 
@@ -79,65 +81,53 @@ def main(auto_save=False):
         print(
             f"Generation {gen}\n  Best fitness: {max(fitnesses)}\n  Avg fitness: {np.mean(fitnesses)}\n"
         )
+        save_best(population, fitnesses, gen, fp)
+        log(fitnesses, gen, fp)
 
-    save_and_exit(population, fitnesses, auto_save)
 
-
-def save_and_exit(population, fitnesses, auto_save):
-    if not auto_save:
-        # save the best individuals to a file
-        save = input("\nSave best individuals? (y/n) ")
-        if save.lower() != "y":
-            return
-
-        fp = input("Enter save path (default=best): ")
-        if fp == "":
-            fp = "best"
-
-        # strip trailing slash if there is one
-        if fp[-1] == "/":
-            fp = fp[:-1]
-
-        if os.path.exists(fp):
-            print(f"Warning: {fp} already exists. Moving to {fp}_old")
-            os.rename(fp, f"{fp}_old")
-
-    else:
-        fp = "best"
-
-    os.makedirs(fp, exist_ok=True)
-
+def save_best(population, fitnesses, gen, fp):
     max_fitness = max(fitnesses)
     n = 1
-    for i, (individual, fitness) in enumerate(zip(population, fitnesses)):
+    for individual, fitness in zip(population, fitnesses):
         if fitness == max_fitness:
-            print(f"Saving individual {i}")
-            np.save(f"{fp}/{n}.npy", individual)
+            np.save(f"{fp}/gen{gen}_{n}.npy", individual)
             n += 1
 
     print(f"\nSaved {n - 1} individuals to {fp}")
 
 
+def log(fitnesses, gen, fp):
+    if not os.path.exists(f"{fp}/log.txt"):
+        with open(f"{fp}/log.txt", "w") as f:
+            f.write("generation,min_fitness,avg_fitness,max_fitness")
+
+    with open(f"{fp}/log.txt", "a") as f:
+        f.write(f"{gen},{min(fitnesses)},{np.mean(fitnesses)},{max(fitnesses)}\n")
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-a", "--auto", action="store_true")
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("-a", "--auto", action="store_true")
+    # args = parser.parse_args()
 
-    auto = args.auto
-    if not auto:
-        print("-a/--auto not supplied, will prompt for save path at the end")
-        auto_save = input("Switch to auto save? (y/n) ")
-        if auto_save.lower() == "y":
-            auto = True
-        else:
-            print("Continuing with manual save path...")
+    # auto = args.auto
+    # if not auto:
+    # print("-a/--auto not supplied, manually setting save path")
 
-    if auto:
-        print("Auto save enabled")
+    fp = input(f"Enter path to save logs and individuals (default={DEFUALT_PATH}): ")
+    if fp == "":
+        fp = DEFUALT_PATH
+
+    # strip trailing slash if there is one
+    if fp[-1] == "/":
+        fp = fp[:-1]
+
+    try:
+        os.makedirs(fp, exist_ok=False)
+    except FileExistsError:
         print(
-            "Warning: this will overwrite any existing files in ./best. Back up the dir now if you want to keep them."
+            f'Directory "{fp}" already exists. Please move/delete it or choose a different path.'
         )
+        exit()
 
-    print()
-
-    main(auto)
+    main(fp)
